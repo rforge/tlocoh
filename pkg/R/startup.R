d39a89c55eb933d6b7ad192648bf76631b82253d
ajlyons <ajlyons@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-15 22:06:12 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tlocoh@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/startup.R
@@ -0,0 +1,5 @@
+.onAttach <- function(lib, pkg) {
+    ver <- read.dcf(file.path(lib,pkg,"DESCRIPTION"), "Version")
+    msg <- sprintf("T-LoCoH for R (version %s)\nURL: http://tlocoh.r-forge.r-project.org/", as.character(ver))
+    packageStartupMessage(msg)
+}
